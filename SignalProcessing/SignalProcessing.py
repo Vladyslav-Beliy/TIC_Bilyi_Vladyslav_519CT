655e7c8c3dbc9f7e91f5c4efdb2f8c36900406f1
Vladyslav-Beliy <v.v.bilyi@student.khai.edu>
2023-03-31 19:18:04 +0300

    SignalProcessing3

--- SignalProcessing/SignalProcessing.py
+++ SignalProcessing/SignalProcessing.py
@@ -1,4 +1,5 @@
 import numpy
+import numpy as np
 from scipy import signal, fft
 import matplotlib.pyplot as plt
 
@@ -16,8 +17,8 @@ filtered_signal = signal.sosfiltfilt(parameters_filter, random)
 # первий график
 fig, ax = plt.subplots(figsize=(21/2.54, 14/2.54))
 ax.plot(time_line_ox, filtered_signal, linewidth = 1)
-ax.set_xlabel("Время (секунды) ", fontsize = 14)
-ax.set_ylabel("Амплитуда сигнала ", fontsize = 14)
+ax.set_xlabel("Час (секунды) ", fontsize = 14)
+ax.set_ylabel("Частота сигнала ", fontsize = 14)
 
 
 plt.title("Сигнал с максимальной частотой Fmax = 11", fontsize = 14)
@@ -35,7 +36,7 @@ freq_countdown = fft.fftshift(freq_countdown)
 
 fig, ax = plt.subplots(figsize=(21/2.54, 14/2.54))
 ax.plot(freq_countdown, spectrum, linewidth = 1)
-ax.set_xlabel("Время (секунды) ", fontsize = 14)
+ax.set_xlabel("Частота (Гц) ", fontsize = 14)
 ax.set_ylabel("Амплитуда спектра ", fontsize = 14)
 
 plt.title("Спектр с максимальной частотой Fmax = 11", fontsize = 14)
@@ -57,8 +58,9 @@ for i in range(0, 2):
         ax[i][j].plot(time_line_ox, discrete_signals[s], linewidth=1)
         ax[i][j].grid()
         s += 1
+# третий график
 fig.supxlabel('Час (секунди)', fontsize=14)
-fig.supylabel('Амплітуда сигналу', fontsize=14)
+fig.supylabel('Амплитуда спектру', fontsize=14)
 fig.suptitle(f'Сигнал з кроком дискретизації Dt = {steps}', fontsize=14)
 fig.savefig('./figures/' + 'график 3' + '.png', dpi=600)
 
@@ -77,8 +79,9 @@ for i in range(0, 2):
         ax[i][j].plot(freq_countdown, discrete_spectrums[s], linewidth=1)
         ax[i][j].grid()
         s += 1
+# четвертий график
 fig.supxlabel('Частота (Гц) ', fontsize=14)
-fig.supylabel('Амплітуда спектру', fontsize=14)
+fig.supylabel('Час спектру', fontsize=14)
 fig.suptitle(f'Спектри сигналів з кроком дискретизації Dt = {steps}', fontsize=14)
 fig.savefig('./figures/' + 'график 4' + '.png', dpi=600)
 
@@ -97,6 +100,7 @@ for i in range(0, 2):
         ax[i][j].plot(time_line_ox, filtered_discretes_signal[s], linewidth=1)
         ax[i][j].grid()
         s += 1
+# п'ятий график
 fig.supxlabel('Час (секунди)', fontsize=14)
 fig.supylabel('Амплітуда сигналу', fontsize=14)
 fig.suptitle(f'Відновлені аналогові сигнали з кроком дискретизації Dt = {steps}', fontsize=14)
@@ -109,7 +113,7 @@ for i in range(len(steps)):
     dispersion = numpy.var(E1)
     dispersions.append(dispersion)
     signal_noise.append(numpy.var(filtered_signal) / dispersion)
-
+# шостий график
 fig, ax = plt.subplots(figsize=(21 / 2.54, 14 / 2.54))
 ax.plot(steps, dispersions, linewidth=1)
 ax.set_xlabel('Крок дискретизації', fontsize=14)
@@ -117,7 +121,7 @@ ax.set_ylabel('Дисперсія', fontsize=14)
 plt.title(f'Залежність дисперсії від кроку дискретизації', fontsize=14)
 ax.grid()
 fig.savefig('./figures/' + 'график 6' + '.png', dpi=600)
-
+# седьмой график
 fig, ax = plt.subplots(figsize=(21 / 2.54, 14 / 2.54))
 ax.plot(steps, signal_noise, linewidth=1)
 ax.set_xlabel('Крок дискретизації', fontsize=14)
@@ -125,3 +129,75 @@ ax.set_ylabel('ССШ', fontsize=14)
 plt.title(f'Залежність співвідношення сигнал-шум від кроку дискретизації', fontsize=14)
 ax.grid()
 fig.savefig('./figures/' + 'график 7' + '.png', dpi=600)
+
+bits_list = []
+quantize_signals = []
+num = 0
+for M in [4, 16, 64, 256]:
+    delta = (numpy.max(filtered_signal) - numpy.min(filtered_signal)) / (M - 1)
+    quantize_signal = delta * np.round(filtered_signal / delta)
+    quantize_signals.append(list(quantize_signal))
+    quantize_levels = numpy.arange(numpy.min(quantize_signal), numpy.max(quantize_signal) + 1, delta)
+    quantize_bit = numpy.arange(0, M)
+    quantize_bit = [format(bits, '0' + str(int(numpy.log(M) / numpy.log(2))) + 'b') for bits in quantize_bit]
+    quantize_table = numpy.c_[quantize_levels[:M], quantize_bit[:M]]
+    fig, ax = plt.subplots(figsize=(14 / 2.54, M / 2.54))
+    table = ax.table(cellText=quantize_table, colLabels=['Значення сигналу', 'Кодова послідовність'], loc='center')
+    table.set_fontsize(14)
+    table.scale(1, 2)
+    ax.axis('off')
+    fig.savefig('./figures/' + 'Таблиця квантування для %d рівнів ' % M + '.png', dpi=600)
+    bits = []
+    for signal_value in quantize_signal:
+        for index, value in enumerate(quantize_levels[:M]):
+            if numpy.round(numpy.abs(signal_value - value), 0) == 0:
+                bits.append(quantize_bit[index])
+                break
+
+    bits = [int(item) for item in list(''.join(bits))]
+    bits_list.append(bits)
+    fig, ax = plt.subplots(figsize=(21 / 2.54, 14 / 2.54))
+    ax.step(numpy.arange(0, len(bits)), bits, linewidth=0.1)
+    ax.set_xlabel('Біти', fontsize=14)
+    ax.set_ylabel('Амплітуда сигналу', fontsize=14)
+    plt.title(f'Кодова послідовність при кількості рівнів квантування {M}', fontsize=14)
+    ax.grid()
+    fig.savefig('./figures/' + 'график %d ' % (8 + num) + '.png', dpi=600)
+    num += 1
+
+dispersions = []
+signal_noise = []
+for i in range(4):
+    E1 = quantize_signals[i] - filtered_signal
+    dispersion = numpy.var(E1)
+    dispersions.append(dispersion)
+    signal_noise.append(numpy.var(filtered_signal) / dispersion)
+
+fig, ax = plt.subplots(2, 2, figsize=(21 / 2.54, 14 / 2.54))
+s = 0
+for i in range(0, 2):
+    for j in range(0, 2):
+        ax[i][j].plot(time_line_ox, quantize_signals[s], linewidth=1)
+        ax[i][j].grid()
+        s += 1
+fig.supxlabel('Час (секунди)', fontsize=14)
+fig.supylabel('Амплітуда сигналу', fontsize=14)
+fig.suptitle(f'Цифрові сигнали з рівнями квантування (4, 16, 64, 256)', fontsize=14)
+fig.savefig('./figures/' + 'график 12' + '.png', dpi=600)
+
+fig, ax = plt.subplots(figsize=(21 / 2.54, 14 / 2.54))
+ax.plot([4, 16, 64, 256], dispersions, linewidth=1)
+ax.set_xlabel('Кількість рівнів квантування', fontsize=14)
+ax.set_ylabel('Дисперсія', fontsize=14)
+plt.title(f'Залежність дисперсії від кількості рівнів квантування', fontsize=14)
+ax.grid()
+fig.savefig('./figures/' + 'график 13' + '.png', dpi=600)
+
+fig, ax = plt.subplots(figsize=(21 / 2.54, 14 / 2.54))
+ax.plot([4, 16, 64, 256], signal_noise, linewidth=1)
+ax.set_xlabel('Кількість рівнів квантування', fontsize=14)
+ax.set_ylabel('ССШ', fontsize=14)
+plt.title(f'Залежність співвідношення сигнал-шум від кількості рівнів квантування', fontsize=14)
+ax.grid()
+fig.savefig('./figures/' + 'график 14' + '.png', dpi=600)
+
